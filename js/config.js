62cede6ef70ca0c22a3fc491c9a0d81a79cdf8e4
azieli02 <garrett.ande@gmail.com>
2026-07-09 23:10:35 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.CHOICE_QUEST_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbyUAO8Td4FMHdcusvUlOWn0-vBFWT0lMNV9D0wg-edLIkz2Wmg1J1RQSktXoC6IHhQiag/exec',
-  version: '2.0.2',
+  version: '2.1.0',
   debug: false,
   refreshMs: 15000,
   requestTimeoutMs: 15000
