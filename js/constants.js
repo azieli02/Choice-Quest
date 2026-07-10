b203b6b13258ad7ad7dcc4ef15ea8c049d88cbba
azieli02 <garrett.ande@gmail.com>
2026-07-09 23:45:47 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -11,6 +11,22 @@ const CONSTANTS = Object.freeze({
       'assets/gems/Choice-Quest-Gem-Purple.PNG'
     ]),
     slots: Object.freeze([
+      Object.freeze({ x: 30, y: 41.5, dropX: 28, dropY: -14, rotation: -5, scale: 0.95 }),
+      Object.freeze({ x: 48, y: 40.8, dropX: 47, dropY: -15, rotation: 3, scale: 0.94 }),
+      Object.freeze({ x: 67, y: 41.3, dropX: 69, dropY: -13, rotation: -4, scale: 0.95 }),
+
+      Object.freeze({ x: 22, y: 46.8, dropX: 21, dropY: -13, rotation: 4, scale: 1.02 }),
+      Object.freeze({ x: 40, y: 46.0, dropX: 41, dropY: -14, rotation: -6, scale: 1.03 }),
+      Object.freeze({ x: 58, y: 46.5, dropX: 57, dropY: -15, rotation: 2, scale: 1.01 }),
+      Object.freeze({ x: 76, y: 46.2, dropX: 77, dropY: -13, rotation: 5, scale: 1.02 }),
+
+      Object.freeze({ x: 16, y: 52.0, dropX: 16, dropY: -12, rotation: -3, scale: 1.15 }),
+      Object.freeze({ x: 33, y: 51.2, dropX: 34, dropY: -14, rotation: 5, scale: 1.15 }),
+      Object.freeze({ x: 50, y: 52.5, dropX: 49, dropY: -13, rotation: -5, scale: 1.14 }),
+      Object.freeze({ x: 67, y: 51.6, dropX: 68, dropY: -15, rotation: 4, scale: 1.15 }),
+      Object.freeze({ x: 83, y: 52.3, dropX: 82, dropY: -12, rotation: -2, scale: 1.14 })
+    ])    ]),
+    slots: Object.freeze([
       Object.freeze({ x: 18, y: 37.0, dropX: 18, dropY: -13, rotation: -6, scale: 0.76 }),
       Object.freeze({ x: 30, y: 36.2, dropX: 29, dropY: -15, rotation:  4, scale: 0.78 }),
       Object.freeze({ x: 42, y: 37.4, dropX: 43, dropY: -14, rotation: -3, scale: 0.80 }),
