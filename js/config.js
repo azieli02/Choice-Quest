48bf7d31bf44ae754b48d054a071200da998d4c8
azieli02 <garrett.ande@gmail.com>
2026-07-09 22:21:23 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyUAO8Td4FMHdcusvUlOWn0-vBFWT0lMNV9D0wg-edLIkz2Wmg1J1RQSktXoC6IHhQiag/exec",
-  VERSION: "2.0.1",
-  DEBUG: false
-};
+window.CHOICE_QUEST_CONFIG = Object.freeze({
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyUAO8Td4FMHdcusvUlOWn0-vBFWT0lMNV9D0wg-edLIkz2Wmg1J1RQSktXoC6IHhQiag/exec',
+  version: '2.0.2',
+  debug: false,
+  refreshMs: 15000,
+  requestTimeoutMs: 15000
+});
